1a21c50343e5084e3f3499a9930f35a5abb4c6d5
Alexandre DeFreitas <abd9@njit.edu>
2014-11-27 21:28:32 -0500

    Setup Twitter class file

---
+++ app/class/TwitterController/Twitter.class.php
@@ -0,0 +1,15 @@
+<?php
+  namespace TwitterController;
+
+  class Twitter{
+  
+    private $settings = array(
+      'oauth_access_token' => "2849321127-VTfYJfhIobqLa1CUKSMDaQwv9tRs9bRB0uWGLdq",
+      'oauth_access_token_secret' => "fMmAUgvf0w3RbFDMYrgaBHTucR9Otom5IsDk3nW5W5vCW",
+      'consumer_key' => "cR6YV752XhP8lgvcfEwcQUO4y",
+      'consumer_secret' => "4ZEV91gYD9wxUbprqeUX9lQcaea1IxDu74EAKUBFhRz1nJ1z7L"
+    );
+
+  }
+
+?>
