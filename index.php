ee9b3394ddffd4d61fac2115d835155e37f541db
Alexandre DeFreitas <abd9@njit.edu>
2014-11-27 21:18:30 -0500

    Setup autoloader

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
   ini_set('display_errors', 1);
   error_reporting(-1);
 
+  function my_autoloader($class){
+    $filepath = 'class/' . str_replace("\\", "/", $class) . '.class.php';
+    include $filepath;
+  }
+
+  spl_autoload_register('my_autoloader');
+
   
-
-
 
 
 ?>
