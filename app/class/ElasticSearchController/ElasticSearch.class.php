5077515a4a0556c3ac017b9bec216daafd02fa04
Alexandre DeFreitas <abd9@njit.edu>
2014-12-12 18:48:04 -0500

    created method to search for tweets

--- app/class/ElasticSearchController/ElasticSearch.class.php
+++ app/class/ElasticSearchController/ElasticSearch.class.php
@@ -1,25 +1,37 @@
 <?php
 
+namespace ElasticSearchController;
+
 class ElasticSearch {
   
-  $client;
+  public $client;
 
   function insertTweet($tweetData) {
-    $index = $tweetData['id'];
+    $index = 'public_tweets';
     $type = 'tweet';
-    $id = $tweetData['id'];
-
+    $id = $tweetData['id_str'];
 
     $params = array();
-    $params['body'] = array('text'=>$tweetData['text'], 'user'=>$tweetData['user']['screen_name'], 'time'=>$tweetData['created_at']);
+    $params['body']['text'] = $tweetData['text'];
+    $params['body']['user'] = $tweetData['user']['screen_name'];
+    $params['body']['time'] = $tweetData['created_at'];
+    //$params['body'] = array('text'=>'my_text', 'user'=>'user', 'time'=>'2:00');
     $params['index'] = $index;
     $params['type'] = $type;
     $params['id'] = $id;
-    $client->index($params);
+    
+    $retDoc = $this->client->index($params);
+    return $retDoc;
   }
 
   function searchTweets($searchString) {
-    
+  	$searchParams = array();
+    $searchParams['index'] = 'public_tweets';
+    $searchParams['type'] = 'tweet';
+    $searchParams['body']['query']['wildcard']['text'] = '*' . strtolower($searchString) . '*';
+
+    $retDoc = $this->client->search($searchParams);
+    return $retDoc;
   }
 
 
