8b4c5018d4b396acc933ac8b4a05603a0d88be57
Alexandre DeFreitas <abd9@njit.edu>
2014-12-12 19:06:48 -0500

    made HTML controller

--- app/class/HtmlController/Html.class.php
+++ app/class/HtmlController/Html.class.php
@@ -6,8 +6,9 @@
     
     
 
-    function __construct(){
-      
+    public static function render($page_type){
+      $page_class = 'HtmlController\\' . $page_type;
+      return new $page_class;
     }
 
   }
